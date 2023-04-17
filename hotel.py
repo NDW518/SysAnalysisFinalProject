75d8b6b9e71d6a7b2e053f58d81c8502517e4042
Nicholas Ward <nicholas.ward2020@gmail.com>
2023-04-17 18:12:38 -0400

    Functioning Code

--- hotel.py
+++ hotel.py
@@ -1,4 +1,5 @@
 from flask import Flask, redirect, url_for, render_template, request
+import sqlite3 as sql
 app = Flask(__name__)
 
 @app.route('/')
@@ -9,16 +10,43 @@ def welcome():
 def bookaroom():
 	return render_template('booking.htm')
 
+@app.route("/addbookingrecord", methods = ["POST", "GET"])
+def addbookingrecord():
+    if request.method == "POST":
+        name = request.form["name"]
+        checkin = request.form["checkin"]
+        checkout = request.form["checkout"]
+        roomtype = request.form["roomtype"]
+
+        cmd = "INSERT INTO hotel (name, checkin, checkout, roomtype) VALUES ('{0}', '{1}', '{2}', '{3}')".format(name, checkin, checkout, roomtype)
+
+        with sql.connect("database.db") as conn:
+            cur = conn.cursor()
+            cur.execute(cmd)
+            conn.commit()
+            # msg = "Customer data successfully saved."
+            return render_template("confirmed.htm", name=name, checkin=checkin, checkout=checkout) #, msg = msg
+
 @app.route('/login', methods=['POST', 'GET'])
 def login():
-	if request.method == 'POST':
-		user = request.form['username']
-		pswd = request.form['password']
-		if user == 'manager' and pswd == 'hotelManager18':
-			return render_template('manager_view.htm', name=user)
-		else:
-			return render_template('rejected_login.htm', name=user)
-	return 'login'
+    if request.method == 'POST':
+        user = request.form['username']
+        pswd = request.form['password']
+        if user == 'manager' and pswd == 'hotelManager18':
+#            return render_template('list_bookings.htm', name=user)
+                conn = sql.connect("database.db")
+                conn.row_factory = sql.Row
+
+                cmd = "SELECT * FROM hotel"
+                cur = conn.cursor()
+                cur.execute(cmd)
+                rows = cur.fetchall()
+                conn.close()
+                return render_template("list_bookings.htm", rows = rows)
+        else:
+            return render_template('rejected_login.htm', name=user)
+    else:
+        return render_template('login.htm')
 
 
 if __name__ == "__main__":
